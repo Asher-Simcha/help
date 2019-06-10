9b68a479a142a87f7fa20b9e1763b91dd40cb8fd
Asher Simcha <26181751+Asher-Simcha@users.noreply.github.com>
2019-06-10 06:14:32 -0700

    updated all folders
    Updated all Empty Pages.???
    and added and index.php to each folder

--- public_html/basic/0000_Empty_BASIC_Page.bas
+++ public_html/basic/0000_Empty_BASIC_Page.bas
@@ -1,13 +1,24 @@
 PROGRAM product
-! Title:  
+! Title: 
 ! Author: 
-! Date: 
-! Description: Intro to Basic
+! Additional Authors: 
+! Filename: 
+! Description: 
 ! Version: 
+! Date: 
 ! Last Modified: 
+! Additional_Notes: 
+! Source: 
+! Additional_Sources: 
+! Credits: 
+! Additional Credits: 
 ! Location_of_the_Video: 
-! Meta_data_for_Video: 
-! Web_site_for_video:
+! Embed_YouTube: 
+! Website_For_Video: 
+
+! Add your license here. Here is a good list of licenses, that you can use:
+! https://www.gnu.org/licenses/license-list.html
+
 LET m = 2 ! mass in kilograms
 LET a = 4 ! acceleration in mks units
 LET force = m*a ! force in Newtons
--- public_html/batch/0000_Empty_Batch_Page.bat
+++ public_html/batch/0000_Empty_Batch_Page.bat
@@ -1,15 +1,23 @@
 @echo off
-rem  Title:
-rem  Author:
-rem  Additional Authors:
-rem  Filename:
-rem  Description:
-rem  Version:
-rem  Date:
-rem  Last Modified:
-rem  Location_of_the_Video:
-rem  Meta_data_for_YouTube:
-rem  Web_Site_For_Video:
+rem Title: 
+rem Author: 
+rem Additional Authors: 
+rem Filename: 
+rem Description: 
+rem Version: 
+rem Date: 
+rem Last Modified: 
+rem Additional_Notes: 
+rem Source: 
+rem Additional_Sources: 
+rem Credits: 
+rem Additional Credits: 
+rem Location_of_the_Video: 
+rem Embed_YouTube: 
+rem Website_For_Video: 
+ 
+rem Add your license here. Here is a good list of licenses, that you can use:
+rem https://www.gnu.org/licenses/license-list.html
 
 rem  Start Your Script Here
 
